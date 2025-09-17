e4bc07b4fba23ed46fdc6c3a5518e0fcd87d3cd6
Aerxs666 <camiloeselmaspro123@outlook.com>
2025-09-17 15:07:13 -0500

    clase4

---
+++ Clase4/main.py
@@ -0,0 +1,35 @@
+"""
+def cuadrado(numero):
+    return numero * numero
+print(cuadrado(4))
+print(cuadrado(7))
+"""
+
+
+
+"""
+def sumar(a,b):
+    resultado=a+b
+    return resultado
+print("la suma es: ",sumar(5,3))
+"""
+
+"""
+edad=int(input("Introduce tu edad: "))
+if edad<18:
+    print("Eres menor de edad.")
+else: 
+    print("Eres mayor de edad :D.")
+"""
+
+
+edad=input("Introduce tu edad: ")
+if edad<="9":
+    print("Eres un niño")
+if edad>="10":
+    print("Eres un adolescente")
+if edad>="18":
+    print("Eres un adulto")
+else: print("Eres un adulto mayor")
+
+
